6095c46755cea0a1b256142691a5ef528cac0ac0
coderaiser <coderaiser@cloudcmd.io>
2020-02-18 17:54:40 +0200

    feature(package) madrun v5.4.2

--- .madrun.js
+++ .madrun.js
@@ -3,9 +3,8 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'lint': () => 'eslint bin lib madrun.js',
+    'lint': () => 'putout bin lib .madrun.js',
     'fix:lint': () => run('lint', '--fix'),
-    'putout': () => 'putout bin lib madrun.js',
     'test': () => 'echo "Error: no test specified" && exit 1',
 };
 
